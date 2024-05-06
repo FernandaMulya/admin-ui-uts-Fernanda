f6d35739dbbf0636febdd8c0d9c9d8c21156ed0a
MZainalFuad <mzainalfuad99@gmail.com>
2024-05-06 23:13:11 +0700

    updatt

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formsource";
+
+
+function App() {
+  return (
+          <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users">
+            <Route index element={<List />} />
+            <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+            <Route path=":userId" element={<Single />} />
+          </Route>
+          <Route path="/products">
+            <Route index element={<List />} />
+            <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+            <Route path=":productId" element={<Single />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  
+  );
+}
+
+export default App;
